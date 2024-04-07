ccc008cf4957e027c24f6165de6796f95a5fb341
Ángel Hernán Alberto Patricio Arroyo <129218946+AngelHer2005@users.noreply.github.com>
2024-04-06 22:45:41 -0500

    Add files via upload

---
+++ ENTREGABLE_2/src/CÓDIGO/Empleado.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CÓDIGO;
+
+/**
+ *
+ * @author Alberto Arroyo
+ */
+public class Empleado {
+    public int codigo;
+    public String nombre;
+    private int horas_T;
+    private double tarifa_H;
+
+    public Empleado(int codigo, String nombre, int horas_T, double tarifa_H) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.horas_T = horas_T;
+        this.tarifa_H = tarifa_H;
+    }
+
+    public double extraerSueldoBruto(){
+        double sueldo_B = horas_T * tarifa_H;
+        return sueldo_B;
+    }
+    
+    public double extraerDescuento(){
+        double descuento = extraerSueldoBruto() * 0.15;
+        return descuento;
+    }
+    
+    public double extraerSueldoNeto(){
+        double sueldo_N = extraerSueldoBruto() - extraerDescuento();
+        return sueldo_N;
+    }
+    
+}
---
+++ ENTREGABLE_2/src/CÓDIGO/Principal.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package CÓDIGO;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alberto Arroyo
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        1. Diseñe la Clase Empleado con los atributos públicos: codigo (int), nombre (String), horas
+        trabajadas (int) y tarifa por hora (double).
+        2. Crear los siguientes métodos en la clase “Empleado”:
+        – Un método que retorne el sueldo bruto (horas*tarifa).
+        – Un método que retorne el descuento (15% del sueldo bruto).
+        – Un método que retorne el sueldo neto (sueldo bruto - descuento).
+        3. Crear tres objetos a partir de la clase “Empleado”, asignarle datos y realizar los cálculos del
+        sueldo.
+        */
+        Empleado e1 = new Empleado(4, "Que", 40, 40);
+        Object[] empleados = {new Empleado(1, "Ángel", 40, 20.5),
+                              new Empleado(2, "José", 50, 23.2),
+                              new Empleado(3, "María", 45, 18)};
+        StringBuilder msj = new StringBuilder();
+        
+        for (Object obj : empleados) {
+            if (obj instanceof Empleado empleado) {
+                msj.append("El empleado " + empleado.nombre + ", cuyo código es '" + empleado.codigo
+                        + "' posee un sueldo bruto de S/. " + empleado.extraerSueldoBruto() + ", un descuento de S/. " +
+                        empleado.extraerDescuento() + " y un sueldo neto de S/. " + empleado.extraerSueldoNeto() + ".\n");
+            }
+        }
+        JOptionPane.showMessageDialog(null,
+                                      msj,
+                                      "Mensaje",
+                                      JOptionPane.INFORMATION_MESSAGE);
+    }
+    
+}
